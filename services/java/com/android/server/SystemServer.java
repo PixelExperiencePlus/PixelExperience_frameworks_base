3bb07e7ff30e1638373c4d6a4d348fb7c7addb10
Gegham Zakaryan <zakaryan.2004@outlook.com>
2021-04-04 16:09:33 +0400

    fwb: Add CustomGlobalActions service
    The service will offer updateUserConfig, getUserConfig, and
    userConfigContains methods, which will be used from LineageParts and
    fwb. The service will also observe Settings.Global.BUGREPORT_IN_POWER_MENU
    and Settings.Secure.LOCKDOWN_IN_POWER_MENU to update the GlobalActions
    dialog when these are updated.
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Change-Id: I46663ab1ffe6f8d8b2a3dc7dbe54a65b7a62046a

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -250,6 +250,9 @@ import java.util.concurrent.Future;
 // LineageHardware
 import com.android.server.custom.LineageHardwareService;
 
+// CustomGlobalActionsService
+import com.android.server.custom.globalactions.CustomGlobalActionsService;
+
 /**
  * Entry point to {@code system_server}.
  */
@@ -2607,7 +2610,11 @@ public final class SystemServer implements Dumpable {
             t.traceBegin("StartLineageHardwareService");
             mSystemServiceManager.startService(LineageHardwareService.class);
             t.traceEnd();
-            
+
+            // CustomGlobalActionsService
+            t.traceBegin("StartCustomGlobalActionsService");
+            mSystemServiceManager.startService(CustomGlobalActionsService.class);
+            t.traceEnd();
         }
 
         t.traceBegin("StartMediaProjectionManager");
