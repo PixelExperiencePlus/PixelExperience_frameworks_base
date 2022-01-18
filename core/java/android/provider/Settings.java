0f7a4dec4df101786ee80cd7e41f2a176832368e
jhenrique09 <jhenrique09@users.noreply.github.com>
2022-01-18 20:46:03 +0800

    fwb: Implement cutout force full screen [1/2]
    Inspired by MIUI and Essential
    
    CutoutFullscreenController: Adapted from https://github.com/LineageOS/android_lineage-sdk/blob/lineage-16.0/sdk/src/java/org/lineageos/internal/applications/LongScreen.java
    
    [UtsavBalar1231]: Adapt to android 13
    
    Change-Id: Iacc463f2169f6d6861a4b5e323f4381558b6a423

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5990,6 +5990,13 @@ public final class Settings {
                 "high_touch_polling_rate_enable";
 
         /**
+         * Force full screen for devices with cutout
+         * @hide
+         */
+        @Readable
+        public static final String FORCE_FULLSCREEN_CUTOUT_APPS = "force_full_screen_cutout_apps";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -6149,6 +6156,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK);
             PRIVATE_SETTINGS.add(HIGH_TOUCH_SENSITIVITY_ENABLE);
             PRIVATE_SETTINGS.add(HIGH_TOUCH_POLLING_RATE_ENABLE);
+            PRIVATE_SETTINGS.add(FORCE_FULLSCREEN_CUTOUT_APPS);
         }
 
         /**
