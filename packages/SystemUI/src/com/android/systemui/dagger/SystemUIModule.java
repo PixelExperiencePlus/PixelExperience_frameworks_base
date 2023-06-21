a3f810f6d00864f5638f2b5a1b20b0e04b7df350
jhenrique09 <jhsv09@gmail.com>
2023-06-21 15:02:38 -0300

    SystemUI: Import CustomQSModule
    Change-Id: Id6cbfdbb345fa869b5d902d90714cabbfb70630d

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -135,6 +135,8 @@ import com.android.systemui.wmshell.BubblesManager;
 import com.android.systemui.R;
 import com.android.wm.shell.bubbles.Bubbles;
 
+import com.android.systemui.custom.CustomQSModule;
+
 import dagger.Binds;
 import dagger.BindsOptionalOf;
 import dagger.Module;
@@ -181,6 +183,7 @@ import javax.inject.Named;
             GarbageMonitorModule.class,
             KeyboardModule.class,
             LetterboxModule.class,
+            CustomQSModule.class,
             KeyguardBlueprintModule.class,
             LogModule.class,
             MediaProjectionModule.class,
